2ca92162487bcd409f72c9bf68e7e357283df813
jpmitri <60190789+jpmitri@users.noreply.github.com>
2022-12-21 05:07:40 +0200

    init

--- AtoEFunctions/AtoEFunctions.cs
+++ AtoEFunctions/AtoEFunctions.cs
@@ -4,80 +4,78 @@ using System.Text;
 
 namespace AtoEFunctions
 {
-    public class AtoEFunctions
+
+    public ref struct ArCharacters
     {
-        private ref struct ArCharacters
+        ReadOnlySpan<char> Ar_Init = "ابتثجحخدذرزسشصضطظعغفقكلمنهويىةؤءئ".AsSpan();
+
+        public ArCharacters()
         {
-            ReadOnlySpan<char> Ar_Init = "ابتثجحخدذرزسشصضطظعغفقكلمنهويىةؤءئ".AsSpan();
-
-            public ArCharacters()
-            {
-            }
-            public bool ArToEnChar(String input, out String? En)
-            {
-                En = null;
-                bool retres = false;
-                input.Trim('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
-                StringBuilder EnSB = new(input);
-                ReadOnlySpan<char> Sinput = input.AsSpan();
-                ref char searchSpace = ref MemoryMarshal.GetReference(Sinput);
-                for (int i = 0; i < Sinput.Length; i++)
-                {
-                    char currentchar = Unsafe.Add(ref searchSpace, i);
-                    if (Ar_Init.Contains(currentchar))
-                    {
-                        retres = true;
-
-                        EnSB[i] = currentchar switch
-                        {
-                            'ا' => 'a',
-                            'ب' => 'b',
-                            'ت' => 't',
-                            'ث' => 't',
-                            'ج' => 'j',
-                            'ح' => 'h',
-                            'خ' => '5',
-                            'د' => 'd',
-                            'ذ' => 'z',
-                            'ر' => 'r',
-                            'ز' => 'z',
-                            'س' => 's',
-                            'ش' => '7',
-                            'ص' => 's',
-                            'ض' => 'd',
-                            'ط' => 't',
-                            'ظ' => 'z',
-                            'ع' => '3',
-                            'غ' => '8',
-                            'ف' => 'f',
-                            'ق' => 'k',
-                            'ك' => 'k',
-                            'ل' => 'l',
-                            'م' => 'm',
-                            'ن' => 'n',
-                            'ه' => 'h',
-                            'و' => 'w',
-                            'ي' => 'y',
-                            'ى' => 'a',
-                            'ة' => 'h',
-                            'ؤ' => 'o',
-                            'ء' => 'a',
-                            'ئ' => 'i',
-                            _ => throw new NotImplementedException(),
-                        };
-                    }
-                }
-                if (retres)
-                {
-                    EnSB.Replace("7", "ch");
-                    EnSB.Replace("8", "gh");
-                    EnSB.Replace("5", "kh");
-                    En = EnSB.ToString();
-                }
-
-                return retres;
-            }
         }
+        public bool ArToEnChar(String input, out String? En)
+        {
+            En = null;
+            bool retres = false;
+            input.Trim('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+            StringBuilder EnSB = new(input);
+            ReadOnlySpan<char> Sinput = input.AsSpan();
+            ref char searchSpace = ref MemoryMarshal.GetReference(Sinput);
+            for (int i = 0; i < Sinput.Length; i++)
+            {
+                char currentchar = Unsafe.Add(ref searchSpace, i);
+                if (Ar_Init.Contains(currentchar))
+                {
+                    retres = true;
 
+                    EnSB[i] = currentchar switch
+                    {
+                        'ا' => 'a',
+                        'ب' => 'b',
+                        'ت' => 't',
+                        'ث' => 't',
+                        'ج' => 'j',
+                        'ح' => 'h',
+                        'خ' => '5',
+                        'د' => 'd',
+                        'ذ' => 'z',
+                        'ر' => 'r',
+                        'ز' => 'z',
+                        'س' => 's',
+                        'ش' => '7',
+                        'ص' => 's',
+                        'ض' => 'd',
+                        'ط' => 't',
+                        'ظ' => 'z',
+                        'ع' => '3',
+                        'غ' => '8',
+                        'ف' => 'f',
+                        'ق' => 'k',
+                        'ك' => 'k',
+                        'ل' => 'l',
+                        'م' => 'm',
+                        'ن' => 'n',
+                        'ه' => 'h',
+                        'و' => 'w',
+                        'ي' => 'y',
+                        'ى' => 'a',
+                        'ة' => 'h',
+                        'ؤ' => 'o',
+                        'ء' => 'a',
+                        'ئ' => 'i',
+                        _ => throw new NotImplementedException(),
+                    };
+                }
+            }
+            if (retres)
+            {
+                EnSB.Replace("7", "ch");
+                EnSB.Replace("8", "gh");
+                EnSB.Replace("5", "kh");
+                En = EnSB.ToString();
+            }
+
+            return retres;
+        }
     }
+
 }
--- AtoE_Run/Program.cs
+++ AtoE_Run/Program.cs
@@ -1,1 +1,96 @@
-﻿String Path=Console.ReadLine()??throw new exeption;
+﻿using TagLib;
+
+Console.WriteLine("Please Enter Path");
+String? Path = Console.ReadLine();
+
+while (Path == null || Path == string.Empty)
+{
+    Console.WriteLine("Path Cannot Be Null Or Empty");
+    Console.WriteLine("Please Enter A Valid Path or 0 to exit");
+    Path = Console.ReadLine();
+    if (Path == "0")
+    {
+        Environment.Exit(0);
+    }
+}
+
+while (!Directory.Exists(Path))
+{
+    Console.WriteLine("Path Dose Not Exists");
+    Console.WriteLine("Please Enter A Valid Path or 0 to exit");
+    Path = Console.ReadLine();
+    if (Path == "0")
+    {
+        Environment.Exit(0);
+    }
+}
+
+logic();
+
+
+
+async void logic()
+{
+    String[] Files = Directory.GetFiles(Path, "*.*", SearchOption.AllDirectories)
+        .Where(x => x.ToLower().Contains("mp3") || x.ToLower().Contains("wav")).ToArray();
+
+    for (int i = 0; i < Files.Length; i++)
+    {
+        _ = await ClearTags(Files[i]);
+    }
+}
+
+
+
+
+Task<bool> ClearTags(String FileEntity)
+{
+    try
+    {
+        FileEntity = FileEntity.Replace(@"\\", @"/");
+        var tffile = TagLib.File.Create(FileEntity);
+        tffile.RemoveTags(TagTypes.AllTags);
+        tffile.Save();
+        
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"{FileEntity} {e.Message}");
+        String s = System.IO.File.ReadAllText(@"D:\Tags.txt");
+        using (StreamWriter writer = new(@"D:\Tags.txt"))
+        {
+
+            writer.Write($"{s}\n\n{FileEntity}\n{e.Message}");
+        }
+
+    }
+    ArToEn(FileEntity);
+    return Task.FromResult(true);
+}
+
+void ArToEn(String FileEntity)
+{
+    AtoEFunctions.ArCharacters xyz = new();
+    String? NewPath = FileEntity;
+    if (xyz.ArToEnChar(FileEntity, out NewPath))
+    {
+        if (NewPath != null)
+        {
+            try
+            {
+                System.IO.File.Move(FileEntity, NewPath);
+                Console.WriteLine(NewPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{FileEntity} {e.Message}");
+                String s = System.IO.File.ReadAllText(@"D:\ARtEN.txt");
+                using (StreamWriter writer = new(@"D:\ARtEN.txt"))
+                {
+
+                    writer.Write($"{s}\n\n{FileEntity}\n{NewPath}\n{e.Message}");
+                }
+            }
+        }
+    }
+}
